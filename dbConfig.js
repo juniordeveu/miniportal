578ec54a43986dd9231452163ff228e293b7313c
juniordeveu <122225960+juniordeveu@users.noreply.github.com>
2023-01-10 14:15:35 +0100

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -5,7 +5,7 @@ set conf postgresql
 
 require("dotenv").config();
 const {Pool} = require("pg");
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === "development";
 const connetion = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`; 
 const  pool = new Pool(
     {
